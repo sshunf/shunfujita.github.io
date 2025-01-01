ab1c3f4a073f442ee11c087affcd1ec7fda7e06a
Shun Fujita <sfujit123456@gmail.com>
2025-01-01 20:15:48 +0900

    added fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+  import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+  function App() {
+    return (
+      <>
+        <h1>Shun Fujita</h1>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Undergraduate at Northwestern University, passionate about the intersection of Computer Science and Mathematics. Broad interests in statistics, machine learning, and systems programming.
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      </>
+    )
+  }
 
-export default App
+  export default App
