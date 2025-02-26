4794c9523dc3059a008bebc6ed5ec78dd8caf867
Shun Fujita <sfujit123456@gmail.com>
2025-02-25 22:51:19 -0600

    update collage

--- src/pages/PhotoCollage.tsx
+++ src/pages/PhotoCollage.tsx
@@ -1,19 +1,26 @@
-import Gallery from "react-photo-gallery";
 import aurora from "../images/aurora.jpg";
 import grad from "../images/grad.jpg";
 import lakefill from "../images/lakefill.jpg";
 
 const images = [
-    { src: aurora, width: 4, height: 3},
-    { src: grad, width: 4, height: 3},
-    { src: lakefill, width: 4, height: 3}
-]
+    { src: aurora, height: "row-span-2" },
+    { src: grad, height: "row-span-1" },
+    { src: lakefill, height: "row-span-3" },
+];
+
 const PhotoCollage = () => {
     return (
-        <div className="w-full max-w-xl mx-auto p-4">
-            <Gallery photos={images} />
+        <div className="grid grid-cols-2 gap-4 p-4 max-w-xl mx-auto auto-rows-auto">
+            {images.map((image, index) => (
+                <img 
+                    key={index} 
+                    src={image.src} 
+                    alt={`Photo ${index + 1}`} 
+                    className={`w-full h-auto rounded-lg shadow-md object-cover ${image.height}`}
+                />
+            ))}
         </div>
-    )
+    );
 }
 
 export default PhotoCollage;
