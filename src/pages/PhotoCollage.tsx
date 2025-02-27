0fc46490a89636c4dd21e7d8a0e5cd075f8c4347
Shun Fujita <sfujit123456@gmail.com>
2025-02-26 22:15:54 -0600

    increased width of collage

--- src/pages/PhotoCollage.tsx
+++ src/pages/PhotoCollage.tsx
@@ -1,16 +1,20 @@
 import aurora from "../images/aurora.jpg";
 import grad from "../images/grad.jpg";
 import lakefill from "../images/lakefill.jpg";
+import morningwalk from "../images/morningwalk.jpg"
+import morningwalk2 from "../images/morningwalk2.jpg"
 
 const images = [
     { src: aurora, height: "row-span-2" },
     { src: grad, height: "row-span-1" },
     { src: lakefill, height: "row-span-3" },
+    { src: morningwalk, height: "row-span-2"},
+    { src: morningwalk2, height: "row-span-2"}
 ];
 
 const PhotoCollage = () => {
     return (
-        <div className="grid grid-cols-2 gap-4 p-4 max-w-xl mx-auto auto-rows-auto">
+        <div className="grid grid-cols-2 gap-4 p-4 max-w-4xl w-full mx-auto auto-rows-auto">
             {images.map((image, index) => (
                 <img 
                     key={index} 
