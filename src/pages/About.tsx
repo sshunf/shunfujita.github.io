ed700bdcb003f212bdde7cebd24c24e5a994fa94
Shun Fujita <sfujit123456@gmail.com>
2025-04-12 15:42:04 -0500

    changed introduction

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -6,11 +6,10 @@ function About() {
             <Animation>
                 <h1 className="mb-4 text-[#7d6459]">Shun Fujita</h1>
                 <p className="mx-auto flex max-w-[700px] text-justify text-lg text-[#57443c]">
-                    Hello! My name is Shun Fujita, an undergraduate at Northwestern University,
-                    passionate about the intersection of Computer Science and Mathematics. I was
-                    born and raised in Japan for my whole life (20 years). My broad interests lie
-                    in software development, statistics, machine learning, and systems programming and 
-                    I hope to work in field where I can leverage technology to make an impact.
+                Hi! My name is Shun Fujita, an undergraduate student at Northwestern University, originally from Tokyo, Japan. 
+                With a multicultural background shaped by international experiences, I bring a global perspective to problem-solving 
+                and collaboration. I am particularly excited about computer systems and machine learning, and am actively exploring
+                avenues to work on novel problems and drive impact.
                 </p>
             </Animation>
         </div>
